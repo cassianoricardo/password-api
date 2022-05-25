f333c6f5bb2e598ff2c030e3b109b93d2ee2c66a
cassianoricardo <cassianoricardo@brq.com>
2022-05-25 18:44:00 -0300

    Ajuste nos testes integrados

--- src/test/java/br/com/itau/password/api/integration/PasswordApiIntegrationTest.java
+++ src/test/java/br/com/itau/password/api/integration/PasswordApiIntegrationTest.java
@@ -1,16 +1,20 @@
 package br.com.itau.password.api.integration;
 
 import br.com.itau.password.api.SpringTest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.stream.Stream;
+
 import static br.com.itau.password.api.util.Constantes.PASSWORD;
 import static br.com.itau.password.api.util.Constantes.PATH_IS_VALID;
 import static br.com.itau.password.api.util.Constantes.PATH_PASSWORD;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,81 +23,46 @@ public class PasswordApiIntegrationTest extends SpringTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    @DisplayName("Verifica se a senha passou por todas as regras de validacao")
-    public void validPassword() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get(PATH_PASSWORD.concat(PATH_IS_VALID))
-                        .queryParam(PASSWORD, "Senha123*")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("true"));
+    static Stream<Arguments> passwordsAndExpectedOutputFailProvider() {
+        return Stream.of(
+                arguments("", false),
+                arguments("aa", false),
+                arguments("ab#", false),
+                arguments("AAAbbbCc*", false),
+                arguments("AbTp9!foo", false),
+                arguments("AbTp9!foA", false),
+                arguments("AbTp9 fok", false)
+        );
     }
 
-    @Test
-    @DisplayName("Verifica se a senha está sendo reprovada por não conter ao menos nove caracteres")
-    public void passwordLessThanNineCharacters() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get(PATH_PASSWORD.concat(PATH_IS_VALID))
-                        .queryParam(PASSWORD, "Senha12*")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("false"));
+    static Stream<Arguments> passwordsAndExpectedOutputSuccessProvider() {
+        return Stream.of(
+                arguments("AbTp9!fok", true)
+        );
     }
 
-    @Test
-    @DisplayName("Verifica se a senha está sendo reprovada por não conter ao menos uma letra maiuscula")
-    public void passwordWithoutUpperLetter() throws Exception {
+
+    @MethodSource("passwordsAndExpectedOutputFailProvider")
+    @ParameterizedTest(name = "Verifica se a senha: \"{0}\" é inválida")
+    public void invalidPassword(String password, boolean outputExpected) throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
                         .get(PATH_PASSWORD.concat(PATH_IS_VALID))
-                        .queryParam(PASSWORD, "senha123")
+                        .queryParam(PASSWORD, password)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string("false"));
+                .andExpect(content().string(String.valueOf(outputExpected)));
     }
 
-    @Test
-    @DisplayName("Verifica se a senha está sendo reprovada por não conter ao menos uma letra minuscula")
-    public void passwordWithoutLowerLetter() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get(PATH_PASSWORD.concat(PATH_IS_VALID))
-                        .queryParam(PASSWORD, "SENHA123*")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("false"));
-    }
 
-    @Test
-    @DisplayName("Verifica se a senha está sendo reprovada por conter espaços em branco")
-    public void passwordWithWhiteSpace() throws Exception {
+    @MethodSource("passwordsAndExpectedOutputSuccessProvider")
+    @ParameterizedTest(name = "Verifica se a senha: \"{0}\" é válida")
+    public void validPassword(String password, boolean outputExpected) throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
                         .get(PATH_PASSWORD.concat(PATH_IS_VALID))
-                        .queryParam(PASSWORD, "Senha123 *")
+                        .queryParam(PASSWORD, password)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string("false"));
-    }
-
-    @Test
-    @DisplayName("Verifica se a senha está sendo reprovada por conter caracteres repetidos")
-    public void passwordWithRepeatedCharacter() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get(PATH_PASSWORD.concat(PATH_IS_VALID))
-                        .queryParam(PASSWORD, "Password1*")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("false"));
-    }
-
-    @Test
-    @DisplayName("Verifica se a senha está sendo reprovada por não conter ao menos um dos caracteres: ! @ # $ % ^ & * ( ) - +")
-    public void passwordWithoutSpecialCharacter() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get(PATH_PASSWORD.concat(PATH_IS_VALID))
-                        .queryParam(PASSWORD, "Password1")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("false"));
+                .andExpect(content().string(String.valueOf(outputExpected)));
     }
 
 }
