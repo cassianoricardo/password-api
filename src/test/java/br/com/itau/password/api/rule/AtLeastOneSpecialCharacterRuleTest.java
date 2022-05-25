dbd7ed4cd6a76a403cbec6e0a9e18bad57dce341
cassianoricardo <cassianoricardo@brq.com>
2022-05-25 18:05:19 -0300

    Ajustes no teste unitário de caracteres especiais

--- src/test/java/br/com/itau/password/api/rule/AtLeastOneSpecialCharacterRuleTest.java
+++ src/test/java/br/com/itau/password/api/rule/AtLeastOneSpecialCharacterRuleTest.java
@@ -1,95 +1,66 @@
 package br.com.itau.password.api.rule;
 
 import br.com.itau.password.api.SpringUnitTest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class AtLeastOneSpecialCharacterRuleTest extends SpringUnitTest {
 
     @Autowired
     private AtLeastOneSpecialCharacterRule atLeastOneSpecialCharacterRule;
 
-    @Test
-    @DisplayName("Valida se a senha que possue interrogacao !")
-    void validate_password_with_interrogation() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg!"));
+    static Stream<Arguments> passwordsAndExpectedOutputSuccessProvider() {
+        return Stream.of(
+                arguments("senha!", true),
+                arguments("senha@", true),
+                arguments("senha#", true),
+                arguments("senha$", true),
+                arguments("senha%", true),
+                arguments("senha^", true),
+                arguments("senha&", true),
+                arguments("senha*", true),
+                arguments("senha(", true),
+                arguments("senha)", true),
+                arguments("senha+", true),
+                arguments("senha-", true)
+        );
     }
 
-    @Test
-    @DisplayName("Valida se a senha que possue o caracter de adição +")
-    void validate_password_with_addition() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg+"));
+    static Stream<Arguments> passwordsAndExpectedOutputFailProvider() {
+        return Stream.of(
+                arguments("senha=", false),
+                arguments("senha[", false),
+                arguments("senha]", false),
+                arguments("senha;", false),
+                arguments("senha:", false),
+                arguments("senha|", false),
+                arguments("senha{", false),
+                arguments("senha}", false),
+                arguments("senha¨", false),
+                arguments("senha'", false),
+                arguments("senha§", false),
+                arguments("senha~", false),
+                arguments("senha´", false)
+        );
     }
 
-    @Test
-    @DisplayName("Valida se a senha que possue o caracter de subtracao -")
-    void validate_password_with_subtraction() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg-"));
+    @MethodSource("passwordsAndExpectedOutputSuccessProvider")
+    @ParameterizedTest(name = "Valida se a senha \"{0}\" possue ao menos um dos seguintes caracteres: ! @ # $ % ^ & * ( ) - +")
+    void validate_password_with_specific_special_characters(String password, boolean outputExpected) {
+        assertEquals(atLeastOneSpecialCharacterRule.validate(password), outputExpected);
     }
 
-    @Test
-    @DisplayName("Valida se a senha que possue o parenteses esquerdo (")
-    void validate_password_with_left_parentheses() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg("));
-    }
-
-    @Test
-    @DisplayName("Valida se a senha que possue o parenteses direito )")
-    void validate_password_with_right_parentheses() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg)"));
-    }
-
-    @Test
-    @DisplayName("Valida se a senha que possue o caracter cerquilha #")
-    void validate_password_with_pounds() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg#"));
-    }
-
-    @Test
-    @DisplayName("Valida se a senha que possue o caracter dolar $")
-    void validate_password_with_dollar() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg$"));
-    }
-
-    @Test
-    @DisplayName("Valida se a senha que possue o caracter e comercial &")
-    void validate_password_with_and_commercial() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg&"));
-    }
-
-    @Test
-    @DisplayName("Valida se a senha que possue o caracter de arroba @")
-    void validate_password_with_at_sign() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg@"));
-    }
-
-    @Test
-    @DisplayName("Valida se a senha que possue o caracter de porcentagem %")
-    void validate_password_with_percentage() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg%"));
-    }
-
-    @Test
-    @DisplayName("Valida se a senha que possue o caracter de acento circunflexo ^")
-    void validate_password_with_circumflex_accent() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg^"));
-    }
-
-    @Test
-    @DisplayName("Valida se a senha que possue o caracter de asterisco *")
-    void validate_password_with_asterisk() {
-        assertTrue(atLeastOneSpecialCharacterRule.validate("abcdefg*"));
-    }
-
-
-    @Test
-    @DisplayName("Valida se a senha que não possue ao menos um dos seguintes caracteres: ! @ # $ % ^ & * ( ) - +")
-    void validate_password_without_specific_special_characters() {
-        assertFalse(atLeastOneSpecialCharacterRule.validate("=[];:|{}"));
+    @MethodSource("passwordsAndExpectedOutputFailProvider")
+    @ParameterizedTest(name ="Valida se a senha \"{0}\" não possue ao menos um dos seguintes caracteres: ! @ # $ % ^ & * ( ) - +")
+    void validate_password_without_specific_special_characters(String password, boolean outputExpected) {
+        assertEquals(atLeastOneSpecialCharacterRule.validate(password),outputExpected);
     }
 
 }
